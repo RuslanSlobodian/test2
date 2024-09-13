36bce094ca541566270454c196a29da1010eaf8c
RuslanSlobodian <ruslanslobodian87@gmail.com>
2024-09-13 13:43:49 +0300

    add method toString();

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,6 +1,15 @@
 package com.example;
 
 public class Main {
+    private int a;
+
+    @Override
+    public String toString() {
+        return "Main{" +
+                "a=" + a +
+                '}';
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
